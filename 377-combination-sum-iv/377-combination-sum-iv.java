02995425d7b9eff9d8e5e5abe00d85049be9d70d
SatyamGupta0401 <83504634+SatyamGupta0401@users.noreply.github.com>
2022-03-05 19:10:12 +0530

    Time: 138 ms (5.44%), Space: 61.1 MB (5.41%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int res=0;
+        Arrays.sort(nums);
+        HashMap<String,Integer> m=new HashMap<>();
+      return rec(nums,target,0,res,m);  
+    }
+    int rec(int[] nums,int target,int idx,int res,HashMap<String,Integer> m)
+    {
+        if(target==0)
+            return 1;
+        else if(target<0 || idx==nums.length)
+            return 0;
+    	  String Key=target+"Satyam"+idx;
+        if(m.containsKey(Key))
+        {
+        	return m.get(Key);
+        }
+
+        res=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]<=target)
+            {
+               res=res+rec(nums,target-nums[i],i,res,m);
+            }
+            else
+            {
+            	break;
+            }
+        }
+        //System.out.println("Res is " + res);  
+          m.put(Key,res);       
+        return res;
+    }
+    
+}
