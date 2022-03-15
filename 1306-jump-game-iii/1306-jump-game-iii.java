bce0efbc6dd0f0b473a5596b3c84f49399b634a7
SatyamGupta0401 <83504634+SatyamGupta0401@users.noreply.github.com>
2022-03-15 23:44:42 +0530

    Time: 202 ms (5.06%), Space: 64.8 MB (15.07%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        int l=arr.length;
+        Queue<Integer> q=new LinkedList<>();
+        boolean[] vis=new boolean[l];
+        q.add(start);
+        vis[start]=true;
+        while(!q.isEmpty())
+        {
+            for(int j=0;j<q.size();j++)
+            {
+                int i=q.remove();
+                System.out.println(i);
+                if(arr[i]==0)
+                {
+                    return true;
+                }
+                if(i+arr[i]>=0 && i+arr[i]<l && vis[i+arr[i]]==false)
+                {
+                    
+                  q.add(i+arr[i]);
+                    vis[i+arr[i]]=true;
+                }
+                 if( i-arr[i]>=0 && i-arr[i]<l && vis[i-arr[i]]==false)
+                 {
+                   q.add(i-arr[i]);
+                      vis[i-arr[i]]=true;
+                 }
+            }
+        }
+        return false;
+    }
+}
