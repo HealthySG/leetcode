c0e5b1260972e65f398677331c4b9712c3bf57e2
SatyamGupta0401 <83504634+SatyamGupta0401@users.noreply.github.com>
2022-03-16 10:18:25 +0530

    Time: 210 ms (5.04%), Space: 69.4 MB (8.43%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
         int l=arr.length;
-        Queue<Integer> q=new LinkedList<>();
+        boolean[] vis=new boolean[l];
+      /*  Queue<Integer> q=new LinkedList<>();
         boolean[] vis=new boolean[l];
         q.add(start);
         vis[start]=true;
@@ -28,6 +29,32 @@ class Solution {
                  }
             }
         }
-        return false;
+        */
+        vis[start]=true;
+        return rec(start,arr,vis,l);
+    }
+    boolean rec(int i,int arr[],boolean[] vis,int l)
+    {
+         boolean left=false;
+        boolean right=false;
+         System.out.println("i and arr[i] "+i+" "+arr[i]);
+         if(arr[i]==0)
+          {
+             System.out.println("righ result "+i);
+             return true;
+          }
+        if( i-arr[i]>=0 && i-arr[i]<l && vis[i-arr[i]]==false)
+         {
+             vis[i-arr[i]]=true;
+              left= rec(i-arr[i],arr,vis,l);
+         }
+        if(i+arr[i]>=0 && i+arr[i]<l && vis[i+arr[i]]==false)
+        {
+            vis[i+arr[i]]=true;
+             right= rec(i+arr[i],arr,vis,l);
+            
+        }
+         
+         return left || right;
     }
 }
