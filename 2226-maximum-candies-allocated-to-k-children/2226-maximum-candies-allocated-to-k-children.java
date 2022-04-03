74c4ece24fbc1a6de75fa1d1e2eaf29e35910b3e
SatyamGupta0401 <83504634+SatyamGupta0401@users.noreply.github.com>
2022-04-03 13:36:01 +0530

    Time: 75 ms (33.33%), Space: 78.7 MB (66.67%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -1,27 +1,16 @@
 class Solution {
     public int maximumCandies(int[] candies, long k) {
         int l=candies.length;
-        
         int high=Arrays.stream(candies).max().getAsInt();
-       // long sum=Arrays.stream(candies).sum();
-       // System.out.println(sum);
-       // if(sum<k)
-        //    return 0;
         int low=1,ans=0;
         while(low<=high)
         {
             int mid=(low+high)>>1;
-           //  System.out.println(mid);
-            if(isPossible(mid,candies,k))
-            {
+            if(isPossible(mid,candies,k)){
                 ans=mid;
-                low=mid+1;
-                
+                low=mid+1;  
             }
-            
-            else
-            {
-               // ans=high;
+            else{
                 high=mid-1;
             }
         }
@@ -30,19 +19,13 @@ class Solution {
     boolean isPossible(int mid,int[] candies,long k)
     {
         long f=0;
-        for(int i=0;i<candies.length;i++)
-        {
-                 f+=candies[i]/mid;
-               
-               
+        for(int i=0;i<candies.length;i++){
+                 f+=candies[i]/mid;      
         }
-       // System.out.println(f);
-        if(f>=k)
-        {
+        if(f>=k){
            return true; 
         }
-        else
-        {
+        else{
             return false;
         }
     }
