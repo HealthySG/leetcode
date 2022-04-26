e26715e8116403a388bf625198491b2edddcf533
SatyamGupta0401 <83504634+SatyamGupta0401@users.noreply.github.com>
2022-04-26 19:54:59 +0530

    Time: 10 ms (58.35%), Space: 54.3 MB (62.68%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -1,6 +1,50 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int i = 0, j = s.length() - 1;
+        /* int i=0,j=s.length()-1,c=0,temp1=0,temp2=0;
+        boolean flag=false;
+        if(s.length()==2)
+        {
+            if(s.charAt(0)==s.charAt(1))
+                return true;
+        }
+        else if(s.length()==1)
+            return true;
+        while(i<j)
+        {
+            if(s.charAt(i)==s.charAt(j))
+            {
+                i++;
+               // System.out.println("i+ j" +i);
+                j--;
+                // System.out.println("i+ j" +j);
+            }
+            else if(s.charAt(i)!=s.charAt(j) && c==0)
+            {
+                temp1=i;temp2=j;
+                if(s.charAt(++temp1)==s.charAt(j))
+                    i++;
+                else
+                    j--;
+              //  System.out.println("i+ j" +i);
+                c++;
+               // System.out.println("i+ j" +j);
+            }
+             else if(s.charAt(i)!=s.charAt(j) && c!=0)
+            {
+                 flag=true;
+                 break;
+            }
+            
+        }
+        if(s.length()==3 && c==1)
+        {
+            flag=true;
+        }
+        if(flag==true)
+            return false;
+        else
+            return true;*/
+         int i = 0, j = s.length() - 1;
         while (i < j && s.charAt(i) == s.charAt(j)) {
             i++; j--;
         }
@@ -18,6 +62,6 @@ class Solution {
             }
             else return false;
         }
-        return true; 
+        return true;
     }
 }
