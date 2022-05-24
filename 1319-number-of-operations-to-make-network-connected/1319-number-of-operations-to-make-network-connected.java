b55ccfcb5069dc5baee6e40d29c553c12dbff622
HealthySG <83504634+HealthySG@users.noreply.github.com>
2022-05-24 21:46:48 +0530

    Time: 938 ms (5.03%), Space: 77 MB (20.40%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+      int parent[] = new int[n];
+	  for(int i =0; i <n; i++)
+		  parent[i] = i;
+      int cycle=0,unconnectedcomp=0;
+        for(int[] l:connections)
+        {
+            int from=l[0];
+            int to=l[1];
+            
+            System.out.println("From parent-- " + find(from,parent));
+            System.out.println("to parent-- " + find(to,parent));
+            if(find(from,parent)==find(to,parent))
+            {
+                cycle++;
+            }
+            System.out.println("Cycle--"+ cycle);
+            union(from, to, parent);
+            
+        }
+        for(int i=0;i<n;i++) {
+            if(parent[i] == i) unconnectedcomp++;
+        }
+          if(cycle>=unconnectedcomp-1)
+            {
+                return unconnectedcomp-1;
+            }
+            else
+            {
+                return -1;
+            }
+         //return -1;
+    }
+    public void union(int from, int to, int[] parent)     {
+        from = find(from, parent);
+        to = find(to,parent);
+        
+        parent[to] = from;
+    }
+    public int find(int from,int[] parent)
+    {
+        if(parent[from]==from)
+            return from;
+        return find(parent[from],parent);
+    }
+}
