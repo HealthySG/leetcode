65b7ea8fce66e1c285b6703d1d0038f6f16fd789
HealthySG <83504634+HealthySG@users.noreply.github.com>
2022-07-11 15:19:57 +0530

    Time: 1 ms (95.06%), Space: 42.3 MB (78.00%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -14,21 +14,23 @@
  * }
  */
 class Solution {
-    Set<Integer> st=new HashSet<>();
+  //  Set<Integer> st=new HashSet<>();
     List<Integer> l=new LinkedList<>();
     public List<Integer> rightSideView(TreeNode root) {
        // Queue<TreeNode> q=new LinkedList<>();
         rec(root,0);
         return l;
     }
+   /* 1)-The same thing can also be achieved by comparing the size of the ans list to the current level without using a hashset.
+2)-because each level print only one node.*/
    void rec(TreeNode root,int level)
     {
         if(root==null)
             return;
-        if(!st.contains(level) &&  root!=null)
+        if(l.size()==level) //
         {
              l.add(root.val);
-             st.add(level);
+            // st.add(level);
         }
         
         rec(root.right,level+1);
