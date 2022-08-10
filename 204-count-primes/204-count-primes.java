8808d6205d713f3d9f0912ffc0232e31da2302fb
HealthySG <83504634+HealthySG@users.noreply.github.com>
2022-08-10 22:07:42 +0530

    Time: 172 ms (63.59%), Space: 68.6 MB (47.32%) - LeetHub

---
+++ 204-count-primes/204-count-primes.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countPrimes(int n) {
+       // if
+        boolean[] prime=new boolean[n+1];
+        for(int i=0;i<=n;i++)
+            prime[i]=true;
+        //int g=(int)Math.sqrt(n);
+        for(int i=2;i*i<=n;i++)
+        {
+            if(prime[i]==true)
+            {
+                for(int j=i*i;j<=n;j+=i)
+                {
+                   prime[j]=false; 
+                }
+            }
+        }
+        int ans=0;
+        for(int i=2;i<n;i++)
+        {
+            if(prime[i]==true)
+                ans++;
+        }
+        return ans;
+    }
+}
