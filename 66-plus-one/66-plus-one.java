60bfd1f0d42c6c00e57b3ceba38fe1d52383c677
HealthySG <83504634+HealthySG@users.noreply.github.com>
2022-08-16 23:37:15 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (90.70%) - LeetHub

---
+++ 66-plus-one/66-plus-one.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n=digits.length;
+        int carry=0,a=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(i==n-1)
+            {
+                 a=(digits[i]+1)+carry;
+               //  System.out.println(a);
+            }
+              
+            else
+            {
+                  a=digits[i]+carry;
+               //  System.out.println(a);
+            }
+              
+            carry=(a/10);
+            digits[i]=a%10;
+        }
+        int temp[]=new int[n+1];
+        
+        if(carry!=0)
+        {
+            temp[0]=carry;
+            for(int i=1;i<=n;i++)
+            {
+                temp[i]=digits[i-1];
+                
+            }
+            return temp;
+        }
+          
+       return digits;
+    }
+}
